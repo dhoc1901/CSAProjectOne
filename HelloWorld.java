3308a30f0c1acf4844be30d60ce421c9d0b3b450
David <dhoc1901@mpsedu.org>
2018-09-21 11:51:43 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author David
+ * @version 9/5/18
+ */
+public class HelloWorld
+{
+    public static void main() 
+    {System.out.println("Hello World!");
+    }
+
+}
